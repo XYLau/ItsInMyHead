62043fbfbe5004bba1293c0444b65f20d83edb7d
HyukSoa <haezxq@gmail.com>
2017-03-14 21:34:50 +0800

    checkbox done

--- datagen/search.py
+++ datagen/search.py
@@ -8,21 +8,39 @@ from datagen.models import *
 # search using post
 def search_post(request):
     ctx = {}
-    para = {}
     if request.POST:
-        para[1] = request.POST['country']
-        para[2] = request.POST['code']
+        check_list = request.POST.getlist('check_list')
 
         # search in database
-
-        # country_result = Country.objects.get(countryCode=para[2])
-
+        # Example of select: country_result = Country.objects.get(countryCode=para[2])
         country_all = Country.objects.all()
 
         ctx['rlt'] = ""
+
+        if "name" in check_list:
+            ctx['rlt'] += "Name Selected!" + "<br/>"
+            # TODO: select name from database
+
+        if "gender" in check_list:
+            ctx['rlt'] += "Gender Selected!" + "<br/>"
+            gender = request.POST['gender']
+            # TODO: select gender from database
+
+        if "country" in check_list:
+            ctx['rlt'] += "Country Selected!" + "<br/>"
+            country = request.POST['country']
+            # TODO: select country from database
+
+        if "race" in check_list:
+            ctx['rlt'] += "Race Selected!" + "<br/>"
+            race = request.POST['race']
+            # TODO: select race from database
+
+        if "address" in check_list:
+            ctx['rlt'] += "Address Selected!" + "<br/>"
+            # TODO: select address from database
+
         for item in country_all:
             ctx['rlt'] += (item.countryCode + " " + item.countryName) + "<br/>"
 
-        # value to display in generate.html
-        # ctx['rlt'] = country_result.countryCode + " " + country_result.countryName
     return render(request, "generate.html", ctx)
