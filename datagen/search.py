84e9654af799e7e95f65ef9be713427fa995fd77
HyukSoa <haezxq@gmail.com>
2017-03-15 20:10:41 +0800

    data generation with name
    complete data generation with name selected

--- datagen/search.py
+++ datagen/search.py
@@ -3,44 +3,74 @@ from django.shortcuts import render_to_response, render
 from django.views.decorators import csrf
 
 from datagen.models import *
+import copy
 
 
 # search using post
 def search_post(request):
-    ctx = {}
+    display = []
     if request.POST:
         check_list = request.POST.getlist('check_list')
 
-        # search in database
-        # Example of select: country_result = Country.objects.get(countryCode=para[2])
-        country_all = Country.objects.all()
+        # deal with gender filter
+        if "gender" in check_list:
+            # display.append("Gender Selected!")
+            gender = request.POST['gender']
 
-        ctx['rlt'] = ""
+        # deal with country filter
+        if "country" in check_list:
+            # display.append("Country Selected!")
+            country = request.POST['country']
+            if country:
+                country_obj = Country.objects.filter(countryName=country)
+            else:
+                country_obj = Country.objects.all()
+
+        # race filter
+        if "race" in check_list:
+            # display.append("Race Selected!")
+            race = request.POST['race']
+            if race:
+                race_obj = Race.objects.get(raceDesc=race)
 
         if "name" in check_list:
-            ctx['rlt'] += "Name Selected!" + "<br/>"
-            # TODO: select name from database
-
-        if "gender" in check_list:
-            ctx['rlt'] += "Gender Selected!" + "<br/>"
-            gender = request.POST['gender']
-            # TODO: select gender from database
-
-        if "country" in check_list:
-            ctx['rlt'] += "Country Selected!" + "<br/>"
-            country = request.POST['country']
-            # TODO: select country from database
-
-        if "race" in check_list:
-            ctx['rlt'] += "Race Selected!" + "<br/>"
-            race = request.POST['race']
-            # TODO: select race from database
+            # display.append("Name Selected!")
+            first_name_obj = FirstName.objects.all()
+            last_name_obj = LastName.objects.all()
 
         if "address" in check_list:
-            ctx['rlt'] += "Address Selected!" + "<br/>"
-            # TODO: select address from database
+            display.append("Address Selected!")
+            # TODO: select. wait until address model complete
 
-        for item in country_all:
-            ctx['rlt'] += (item.countryCode + " " + item.countryName) + "<br/>"
+        # Select from db
 
-    return render(request, "generate.html", ctx)
+        # if name is selected
+        if "name" in check_list:
+            if "race" in check_list and race:
+                first_name_obj = first_name_obj.filter(raceId_id=race_obj.raceId)
+            if "gender" in check_list and gender:
+                first_name_obj = first_name_obj.filter(gender=gender)
+            for item_first in first_name_obj:
+                race_id = item_first.raceId_id
+
+                for item_last in last_name_obj.filter(raceId_id=race_id):
+                    attr = dict()
+                    # attr['FirstName'] = item_first.firstName
+                    # attr['LastName'] = item_last.lastName
+                    attr['Name'] = item_first.firstName + " " + item_last.lastName
+
+                    if "race" in check_list:
+                        attr['Race'] = Race.objects.get(raceId=race_id).raceDesc
+                    if "gender" in check_list:
+                        attr['Gender'] = item_first.gender
+
+                    if "country" in check_list:
+                        for item_country in country_obj:
+                            attr_country = copy.copy(attr)
+                            attr_country['CountryName'] = item_country.countryName
+                            attr_country['CountryCode'] = item_country.countryCode
+                            display.append(attr_country)
+                    else:
+                        display.append(attr)
+
+    return render(request, "generate.html", {'datas': display})
