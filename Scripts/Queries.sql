35b3885dcd9eb30922fb8fb5fb735f0e79a397a0
XY Lau <xylau@u.nus.edu>
2017-04-07 06:01:17 +0800

    Update missing primary key from LastName table

--- Scripts/Queries.sql
+++ Scripts/Queries.sql
@@ -1,1 +1,18 @@
 ﻿SELECT firstName FROM FirstName LIMIT 10;
+
+-- Name
+SELECT f.firstName, l.lastName, f.raceId
+FROM FirstName f, LastName l
+WHERE f.raceId = l.raceId
+
+-- Name + Gender
+SELECT f.firstName, l.lastName, f.raceId, g.genderDesc
+FROM FirstName f, LastName l, Gender g
+WHERE f.raceId = l.raceId
+AND f.gender = g.gender
+
+-- Name + Country
+SELECT f.firstName, l.lastName, f.raceId, g.genderDesc
+FROM FirstName f, LastName l, Gender g
+WHERE f.raceId = l.raceId
+AND f.gender = g.gender
